b1377416ba9d28682237ef6bcdc3e2760b784eb3
qianzhong.fu <qianzhong.fu@datayes.com>
2016-04-25 10:35:33 +0800

    add digits

--- python/AddDigits.py
+++ python/AddDigits.py
@@ -1,3 +1,5 @@
+# coding:utf8
+import math
 # Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
 
 # For example:
@@ -7,9 +9,34 @@
 # Follow up:
 # Could you do it without any loop/recursion in O(1) runtime?
 
+# 输入一个整数，如何打印它每一位的数字？
+
+# 123 = 1 * 100 + 2 * 10 + 3 * 1
+
+# num = 123
+# while num > 0:
+#     print "digit is ", num % 10
+#     num /= 10
+
+# 非O(1)算法
+# class Solution(object) :
+#     def addDigits(self, num):
+#         while num >= 10:
+#             num = num / 10 + num % 10
+#         return num
+#余数定理：其中a,b,c都是自然数（0和正整数）
+# 1.余数的加法定理
+#    a与b的和除以c的余数，等于a,b分别除以c的余数之和，或这个和除以c的余数。
+# 2.余数的乘法定理
+#    a与b的乘积除以c的余数，等于a,b分别除以c的余数的积，或者这个积除以c所得的余数。
 #From Wiki
 #DR(n) = 1+(n-1)%9
-class Solution(object):
+# 寻找规律，
+# 1 2 3 4 5 6 7 8 9
+# 10 11 12 13 14 15 16 17 18
+# 19 20 21 22 23 24 25 26 27
+# 注意到了吗，就是一个矩阵，求元素所在列数
+class Solution(object) :
     def addDigits(self, num):
     	if(num == 0):
     		return 0
