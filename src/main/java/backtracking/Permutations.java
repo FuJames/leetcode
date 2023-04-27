6eccae6a6788859ead394fb94381fc9c4b5e8fe5
fuqianzhong <fuqianzhong@tuhu.cn>
2023-04-27 16:09:04 +0800

    全排列-回溯算法

---
+++ src/main/java/backtracking/Permutations.java
@@ -0,0 +1,54 @@
+package backtracking;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 46. 全排列
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ *
+ * 思路：构造决策树，
+ *        1       2    3
+ *       2 3     1 3   1  2
+ *     3     2  3 1   2    1
+ *  第一层，先选择1，再选择2，再选择3
+ *  第二层，假设第一层选择2，可以再选择到1，如何能够选择到1 ？ 是否可以每次从第0位开始选，如果碰到跟2这位一致，则跳过；
+ *  如何判定跟2一致，是否可以转化为路径列表.contains(2)；
+ */
+public class Permutations {
+    LinkedList<Integer> track = new LinkedList<>();
+    LinkedList<List<Integer>> result = new LinkedList<>();
+
+    public static void main(String[] args) {
+        Permutations p = new Permutations();
+        p.permute(new int[]{1,2,3,4});
+        for (List<Integer> l : p.result){
+            for (int i = 0; i < l.size(); i++) {
+                System.out.print(l.get(i));
+            }
+            System.out.println();
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        if(track.size() == nums.length){//精髓 ！！
+            result.addLast(new LinkedList<>(track));
+            return result;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(track.contains(nums[i])){
+                continue;
+            }
+            track.addLast(nums[i]);
+//            permute(nums,i+1);
+            permute(nums);
+            track.removeLast();
+        }
+        return result;
+    }
+}
